9dedb6ce63685517e11a43c8ab71a7e4f0ffb472
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2023-11-25 13:38:02 +0530

    Update newdef.py

--- newdef.py
+++ newdef.py
@@ -1,6 +1,6 @@
 def print_scorecard(team1, team2, score1, score2, goal_scorers, onehasball):
     # Specify the width for each section
-    width = 30
+    width = 50
 
     # Calculate the space needed for team names and scores
     team_score_width = width - len(team1) - len(team2) - len(str(score1)) - len(str(score2))
@@ -8,7 +8,7 @@ def print_scorecard(team1, team2, score1, score2, goal_scorers, onehasball):
     # Print the top section
     print('=' * width)
     print(
-        f"{team1:<{team_score_width // 2}}  {score1:^{len(str(score1))}} - {score2:^{len(str(score2))}} {team2:^{team_score_width // 2}}")
+        f"{team1:<{team_score_width // 2}}  {score1:^{len(str(score1))}} - {score2:<{len(str(score2))}} {team2:^{team_score_width // 2}}")
     print('=' * width)
 
     # Print goal scorers under respective team names
@@ -23,8 +23,6 @@ def print_scorecard(team1, team2, score1, score2, goal_scorers, onehasball):
             print(f"                {scorer['player']} - {scorer['time']}'\n")
     if onehasball==True:
         print("⚽")
-    elif onehasball==False:
-        print("             ⚽")
 
 # Example usage
 team1_name = "Team A"
